15c76c5bfab0521aabae66edc2709f769ed0b9a4
pestrstr <giuseppestracquadanio2@gmail.com>
2022-12-02 19:29:49 +0100

    Add NCSN++ GAN Generator and other support layers

--- models/discriminator.py
+++ models/discriminator.py
@@ -12,19 +12,7 @@ import math
 import torch.nn.functional as F
 import up_or_down_sampling
 from dense_layer import dense, conv2d
-
-def get_sinusoidal_positional_embedding(timesteps, embedding_dim, max_positions=10000):
-  assert len(timesteps.shape) == 1  # and timesteps.dtype == tf.int32
-  half_dim = embedding_dim // 2
-  # magic number 10000 is from transformers
-  emb = math.log(max_positions) / (half_dim - 1)
-  emb = torch.exp(torch.arange(half_dim, dtype=torch.float32, device=timesteps.device) * -emb)
-  emb = timesteps.float()[:, None] * emb[None, :]
-  emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
-  if embedding_dim % 2 == 1:  # zero pad
-    emb = F.pad(emb, (0, 1), mode='constant')
-  assert emb.shape == (timesteps.shape[0], embedding_dim)
-  return emb
+from layers import get_timestep_embedding as get_sinusoidal_positional_embedding
 
 class TimestepEmbedding(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, output_dim, act=nn.LeakyReLU(0.2)):
