1efc22ffc56ec31bd45f6ee34ef0b050282df1e8
pestrstr <giuseppestracquadanio2@gmail.com>
2022-12-05 01:54:06 +0000

    add utils

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import sys
 import os
+import glob
 
 def load_VAE_Model(f=4, d=3, ppath="./lightning_logs"):
     model_path = "{path}/f={a}_d={b}/checkpoints".format(path=ppath, a=f, b=d)
-    
+    checkpoint = glob.glob(model_path)[0]
+    print(checkpoint)
+
+if __name__ == '__main__':
+    ## Use this for testing
+    load_VAE_Model()
 
